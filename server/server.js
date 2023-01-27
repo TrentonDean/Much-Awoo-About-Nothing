a873f1a2d6abca06db8a788c328279801f145be7
smalltimebenny <ben.lindquist@gmail.com>
2023-01-27 10:25:39 -0500

    added dependencies, update server, add mongoose config

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const PORT = 8000
-
-
+require("dotenv").config()
+const cookieParser = require("cookie-parser")
 require("./config/mongoose.config")
+
+app.use(cookieParser())
 app.use(express.json(), express.urlencoded({extended:true}))
-app.use(cors())
+app.use(cors({credentials:true, origin:"http://localhost:3000"}))
 
 
 const AllMyUserRoutes = require("")
