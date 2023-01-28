1a1a57a68a977bb7826547647eaf4d629c70ee80
smalltimebenny <ben.lindquist@gmail.com>
2023-01-28 16:41:29 -0500

    add routing and NavBar to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useNavigate } from 'react';
+import NavBar from './components/NavBar';
+
+
 
 function App() {
+  const [currentUser, setCurrentUser] =useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+        <NavBar setCurrentUser={setCurrentUser} currentUser={currentUser} />
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useNavigate} from "react-router-dom"
+
+const NavBar = (props) => {
+    const navigate = useNavigate()
+  return (
+    <div>
+        <h1>Much Awoo About Nothing</h1>
+        <button></button>
+    </div>
+  )
+}
+
+export default NavBar
