9afae4ae6d1915fad4500b4f8b15ba5ca332ce9e
smalltimebenny <ben.lindquist@gmail.com>
2023-01-30 16:25:19 -0500

    add assests folder

---
+++ client/src/components/LeftPanel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LeftPanel = () => {
+    // need to fetch user/dog info here
+
+  return (
+    <div>
+        <img></img>
+    </div>
+  )
+}
+
+export default LeftPanel
