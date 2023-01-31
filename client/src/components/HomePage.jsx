c2da0ba6589952d07a15a5c667630763b0eafb73
smalltimebenny <ben.lindquist@gmail.com>
2023-01-31 13:08:50 -0500

    add get all posts and iteration functionality to homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -6,38 +6,37 @@ import RightPanel from "./RightPanel";
 
 const HomePage = (props) => {
     const {currentUser, setCurrentUser} =props;
+    const [bodyPosts, setBodyPosts] =useState([]);
 
     useEffect(()=>{
         //get user data if logged in
     })
 
     useEffect(()=>{
-        //get posts, display in reverse chrono order (can we get that in an axios request?)
+        axios.get("http://localhost:8000/api/getAllPosts/")
+        .then(res=>{
+            let postArray = res.data
+            let chronOrderPosts = postArray.sort(
+                (a,b)=>(a.createdAt<b.createdAt) ? 1 : (a.createdAt>b.createdAt) ? -1 :0
+            )
+            setBodyPosts(chronOrderPosts)
+        }
+        )
     })
     
   return (
     <div>
         <LeftPanel />
         <div id="postContainer">
-            //iterate on posts here
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut tellus. Magna etiam tempor orci eu. Diam volutpat commodo sed egestas egestas. Dictum non consectetur a erat nam. Id donec ultrices tincidunt arcu non sodales neque sodales ut. Ut tellus elementum sagittis vitae et leo. Vulputate mi sit amet mauris commodo quis imperdiet. Morbi tempus iaculis urna id volutpat lacus laoreet non. Bibendum enim facilisis gravida neque convallis a cras semper.</p>
+            {bodyPosts.map((item,id)=>{
+                return(
+                    <div>
+                        <p>{item.title}</p>
+                        <p>{item.body}</p>
+                        <p>{item.tags}</p>
+                    </div>
+                )
+            })}
             
         </div>
         <RightPanel />
