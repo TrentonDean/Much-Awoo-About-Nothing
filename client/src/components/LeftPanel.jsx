3f886c994b61ce8884a611f9b9fe2536c4d43f01
smalltimebenny <ben.lindquist@gmail.com>
2023-02-01 01:30:23 -0500

    add func to left panel

--- client/src/components/LeftPanel.jsx
+++ client/src/components/LeftPanel.jsx
@@ -3,20 +3,21 @@ const userPic = require("../assets/user_pfp.png")
 const dogPic = require("../assets/dog_pfp.png")
 
 
-const LeftPanel = () => {
-    // need to fetch user/dog info here
+const LeftPanel = (props) => {
+  const {currentUser, setCurrentUser} =props;
+    // theoretically, currentUser should give all the info it needs.
 
   return (
     <div id="leftPanelContainer">
         <div class="userInfoSide">
             <img class="userImage" alt="the current user's face" src={userPic}></img>
-            <p class="userNameSide">Writer McWriterson</p>
-            <p class="userBioSide">Writer bio goes here.</p>
+            <p class="userNameSide">{currentUser.name}</p>
+            <p class="userBioSide">{currentUser.bio}</p>
         </div>
         <div class="dogInfoSide">
             <img class="dogImage" alt="the current user's dog" src={dogPic}></img>
-            <p class='dogName'>Dog McWriterson</p>
-            <p class='dogBio'>Dog bio goes here.</p>
+            <p class='dogName'>{currentUser.dogs[0].name}</p>
+            <p class='dogBio'>{currentUser.dogs[0].bio}</p>
         </div>
     </div>
   )
