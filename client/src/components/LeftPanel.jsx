e622ac9a96f74f7c4c11370ae1803ffad24f6668
smalltimebenny <ben.lindquist@gmail.com>
2023-02-01 02:04:40 -0500

    finish panel functionality

--- client/src/components/LeftPanel.jsx
+++ client/src/components/LeftPanel.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 const userPic = require("../assets/user_pfp.png")
 const dogPic = require("../assets/dog_pfp.png")
 
 
 const LeftPanel = (props) => {
   const {currentUser, setCurrentUser} =props;
+  const [dummyUser, setDummyUser] =useState({
+    name: "This could be you",
+    bio: "Your story here",
+    dogs:[{
+      name: "The Cutest",
+      bio: "Tell us your dog's story!"
+    }]
+  })
     // theoretically, currentUser should give all the info it needs.
 
+    if (currentUser!==null){
+      setDummyUser(currentUser)
+    }
+  console.log(dummyUser)
   return (
     <div id="leftPanelContainer">
         <div class="userInfoSide">
             <img class="userImage" alt="the current user's face" src={userPic}></img>
-            <p class="userNameSide">{currentUser.name}</p>
-            <p class="userBioSide">{currentUser.bio}</p>
+            <p class="userNameSide">{dummyUser.name}</p>
+            <p class="userBioSide">{dummyUser.bio}</p>
         </div>
         <div class="dogInfoSide">
             <img class="dogImage" alt="the current user's dog" src={dogPic}></img>
-            <p class='dogName'>{currentUser.dogs[0].name}</p>
-            <p class='dogBio'>{currentUser.dogs[0].bio}</p>
+            <p class='dogName'>{dummyUser.dogs[0].name}</p>
+            <p class='dogBio'>{dummyUser.dogs.bio}</p>
         </div>
     </div>
   )
