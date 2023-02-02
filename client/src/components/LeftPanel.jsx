408c07e933cae3a38adb83b46b0b4def25814662
Aimee Young <aim.mikoto@gmail.com>
2023-02-02 13:36:24 -0800

    finished stylization for profile, left,right panel

--- client/src/components/LeftPanel.jsx
+++ client/src/components/LeftPanel.jsx
@@ -31,7 +31,17 @@ console.log("left panel",currentUser)
         <p>{currentUser.dogs}</p>
         </span>:<span>
         <p>Your info here!</p>
-        </span>}
+      </span>}
+
+      {/* Testing Style */}
+      {/* <span>
+        <img className="userImage" src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAOVBMVEX/AAD/oKD/o6P/q6v/tLT/v7//y8v/paX/vLz/39//mpr/nJz//v7/8/P/5eX/2dn/0dH/rq7/rKyn8lSHAAABdUlEQVR4nO3dSU7EMABFQbu7A8083P+wsGADGxBSFL2kyhf4b2nJksfj0/PL683NsizXy6fzl9vTN/P/Tr86/8Xb5S+Wn6534zL27X73hVNh3hznrSesTGGfwj6FfQr7FPYdofB26wkrm+O09YSVKexT2KewT2Gfwj6FfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9s3Ps28K+xT2KexT2KewT2Gfwj6FfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9insO0Lh/l8MKaxT2KewT2Gfwj6FfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9insU9insO8Ihfv/o2T//8worFPYp7BPYZ/CPoV9CvsU9insU9insE9hn8I+hX0K+xT2Kew7QuH71hNWNsdl6wkrU9insE9hn8I+hX0K+xT2KexT2KewT2Gfwj6FfQr7FPYp7FPYp7BvjuvWE1Y2x7L1hJUp7FPYp7BPYZ/CPoV9CvuOULj3G/DDuLufe/bw+gHO+QcZs4I2swAAAABJRU5ErkJggg=='></img>
+        <p className="userNameSide">FirstName</p>
+        <p className="userBioSide">I'm a dog person</p>
+        <img className="dogImage" src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAOVBMVEX/AAD/oKD/o6P/q6v/tLT/v7//y8v/paX/vLz/39//mpr/nJz//v7/8/P/5eX/2dn/0dH/rq7/rKyn8lSHAAABdUlEQVR4nO3dSU7EMABFQbu7A8083P+wsGADGxBSFL2kyhf4b2nJksfj0/PL683NsizXy6fzl9vTN/P/Tr86/8Xb5S+Wn6534zL27X73hVNh3hznrSesTGGfwj6FfQr7FPYdofB26wkrm+O09YSVKexT2KewT2Gfwj6FfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9s3Ps28K+xT2KexT2KewT2Gfwj6FfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9insO0Lh/l8MKaxT2KewT2Gfwj6FfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9insU9insO8Ihfv/o2T//8worFPYp7BPYZ/CPoV9CvsU9insU9insE9hn8I+hX0K+xT2Kew7QuH71hNWNsdl6wkrU9insE9hn8I+hX0K+xT2KexT2KewT2Gfwj6FfQr7FPYp7FPYp7BvjuvWE1Y2x7L1hJUp7FPYp7BPYZ/CPoV9CvuOULj3G/DDuLufe/bw+gHO+QcZs4I2swAAAABJRU5ErkJggg=='></img>
+        <p className="dogNameSide">DogName</p>
+      </span> */}
+      
     </div>
   )
 }
